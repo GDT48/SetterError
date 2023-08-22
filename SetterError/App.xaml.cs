6078774d3021cce56ef1d507eebe99da92ce33bf
Glenn Taylor <glenn@taylor-zone.co.uk>
2023-08-22 15:33:57 +0100

    Add project files.

---
+++ SetterError/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SetterError
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
